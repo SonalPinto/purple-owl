38626e376d00b54b307bd469b4fdae97ede05ea6
SonalPinto <sonalpinto@gmail.com>
2022-09-04 12:41:04 -0700

    Cleanup prototype v0.1 boot.py

--- firmware/v0.1/boot.py
+++ firmware/v0.1/boot.py
@@ -4,11 +4,3 @@ import supervisor
 # =============================================================
 # KMK base boot.py
 supervisor.set_next_stack_limit(4096 + 4096)
-
-# =============================================================
-# Purple Owl Dev Mode entry
-#
-# Latch the chain and poll the first bit of the scan data.
-# If the key was pressed, then enter CircuitPython Dev Mode
-# Else, turn off all USB enumerations except HID
-#
